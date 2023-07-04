6bafc9bfd6e814dae9b7753a58c74ed1a2cac293
probonopd <probonopd@users.noreply.github.com>
2023-07-04 20:36:54 +0200

    Create build.sh
    [ci skip]

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 22.04 (Jammy) and later
+# https://github.com/SerenityOS/serenity/blob/master/Documentation/BuildInstructions.md
+
+sudo apt install build-essential cmake curl libmpfr-dev libmpc-dev libgmp-dev e2fsprogs ninja-build qemu-system-gui qemu-system-x86 qemu-utils ccache rsync unzip texinfo libssl-dev
+
+sudo apt install gcc-12 g++-12
+
+# build the toolchain
+Meta/serenity.sh rebuild-toolchain
+
+# build and run SerenityOS
+Meta/serenity.sh run
